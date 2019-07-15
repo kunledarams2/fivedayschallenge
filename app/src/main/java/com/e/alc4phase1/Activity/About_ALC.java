dce23c7c73d20c5d89af2868d1c5414f175c3d61
kunledarams2 <Adekunle@1>
2019-07-15 02:20:01 +0100

    update

--- app/src/main/java/com/e/alc4phase1/Activity/About_ALC.java
+++ app/src/main/java/com/e/alc4phase1/Activity/About_ALC.java
@@ -1,11 +1,19 @@
 package com.e.alc4phase1.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.e.alc4phase1.MainActivity;
 import com.e.alc4phase1.R;
@@ -13,26 +21,55 @@ import com.e.alc4phase1.R;
 public class About_ALC extends AppCompatActivity {
 
     WebView webView;
+    Toolbar toolbar;
+
     private static String about_Url="https://andela.com/alc/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about__alc);
 
-        webView=findViewById(R.id.about_Website);
-        setupWebview();
+        webView=findViewById(R.id.about_webview);
 
-    }
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
 
-    private void setupWebview(){
+        webView.setWebViewClient(new WebViewClient(){
+
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                if(handler!=null){
+                    handler.proceed();
+
+                }
+                else {
+                    super.onReceivedSslError(view, null, error);
+                }
+
+            }
+        });
+
         webView.loadUrl(about_Url);
-        webView.setWebViewClient(new WebViewClient());
 
-        WebSettings webSettings=  webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+
+        toolbar=findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(About_ALC.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
+
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/e/alc4phase1/Activity/Profile.java
+++ app/src/main/java/com/e/alc4phase1/Activity/Profile.java
@@ -1,15 +1,32 @@
 package com.e.alc4phase1.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.e.alc4phase1.MainActivity;
 import com.e.alc4phase1.R;
 
 public class Profile extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Profile.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
